095fbf0e7035f7bc6f4cfe823b04123f30939426
KurmangaliyevAzamat <kurmangaliev.azamat1996@gmail.com>
2020-11-24 05:47:01 +0600

    bugs from redactor

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 const  taskText = document.querySelector('#taskText')
-const textAreaTaskText = document.querySelector('#divAreaTaskText')
 const imgTaskText = document.querySelector('#imgTaskText');
-var input = document.body.children[0];
-
 
 taskText.oninput = function () {
-    textAreaTaskText.innerHTML = taskText.value;
+    if (taskText.value!=''){
     imgTaskText.src = "https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{100}&space;\\fn_cs&space;\\large&space;"+taskText.value;
+    }
 };
-
-//textAreaTaskText.innerHTML = textAreaTaskText.value;
